03a622327c5a0acf20c2fed1c1a116c06364c804
saicharan2043 <saikann2043@gmail.com>
2024-02-23 01:08:55 +0530

    add new url

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ dotenv.config({
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://live-chatting-app.netlify.app"],
   },
 });
 
